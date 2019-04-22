01ba3ca9243480bbd02a6a94b3612dd31601f336
L <luciano@laratelli.com>
2019-04-22 12:10:23 -0400

    rename buildQAndR to buildQAndU

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include "matrix.hpp"
 #include "vector.hpp"
 
-std::pair<Matrix, Matrix> buildQAndR(const Matrix& start) {
+std::pair<Matrix, Matrix> buildQAndU(const Matrix& start) {
     Matrix Q = Matrix(start.rowCount(), start.colCount());
     Matrix U = Matrix(start.rowCount(), start.colCount());
 
@@ -28,7 +28,7 @@ std::pair<Matrix, Matrix> buildQAndR(const Matrix& start) {
 
 Matrix QRDecomposition(const Matrix &start) {
     auto oldA = start;
-    auto QU = buildQAndR(oldA);
+    auto QU = buildQAndU(oldA);
     auto Q = QU.first;
     auto oldU = QU.second;
     auto R = Q.transpose() * oldA;
@@ -37,7 +37,7 @@ Matrix QRDecomposition(const Matrix &start) {
     oldU = newU;
     oldA = newA;
     for (int i = 0; i < 1000; ++i) {
-        QU = buildQAndR(oldA);
+        QU = buildQAndU(oldA);
         Q = QU.first;
         oldU = QU.second;
         R = Q.transpose() * oldA;
@@ -50,10 +50,11 @@ Matrix QRDecomposition(const Matrix &start) {
 }
 
 int main(int argc, char ** argv) {
-    int64_t n = (argc < 3) ? 3 : std::stoul(argv[1]);
-    int64_t m = (argc < 3) ? 3 : std::stoul(argv[2]);
+    int64_t n = (argc < 3) ? 3 : std::stol(argv[1]);
+    int64_t m = (argc < 3) ? 3 : std::stol(argv[2]);
     auto N = static_cast<unsigned>(n);
     auto M = static_cast<unsigned>(m);
+
     Matrix A(N, M);
     A(0, 0) = 12.0;
     A(0, 1) = -51.0;
@@ -64,6 +65,15 @@ int main(int argc, char ** argv) {
     A(2, 0) = -4.0;
     A(2, 1) = 24.0;
     A(2, 2) = -41.0;
+
+    std::cout << A << std::endl;
+    std::cout << A.smallestValue() << std::endl;
+    std::cout << A.getTriangular(lower) << std::endl;
+    std::cout << A.getTriangular(lower).smallestValue() << std::endl;
+    std::cout << A.getTriangular(upper) << std::endl;
+    std::cout << A.getTriangular(upper).smallestValue() << std::endl;
+
+
     std::cout << QRDecomposition(A) << std::endl;
 }
 
